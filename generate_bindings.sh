cd51bc41654d08beedcdfd6d9a13c9b78046a305
sam bacha <sam@manifoldfinance.com>
2024-05-31 18:37:36 -0700

    chore(lint): shfmt

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -6,17 +6,16 @@ set -eu
 #
 # CONTRACTS_PATH="./src"
 
-
 if [ "$#" -ne 2 ]; then
 	echo "This script takes 2 arguments - CONTRACT_NAME PACKAGE"
 	exit 1
 fi
 
 need_cmd() {
-  if ! command -v "$1" > /dev/null 2>&1; then
-    echo "need '$1' (command not found)"
-    exit 1
-  fi
+	if ! command -v "$1" >/dev/null 2>&1; then
+		echo "need '$1' (command not found)"
+		exit 1
+	fi
 }
 
 need_cmd forge
@@ -32,7 +31,6 @@ PACKAGE=$2
 TYPE_LOWER=$(echo ${TYPE} | tr '[:upper:]' '[:lower:]')
 # FILENAME="${TYPE_LOWER}_deployed.go"
 
-
 mkdir -p dist/
 mkdir -p sdk/go/
 OUTPUT_DIR=sdk/go/
@@ -40,9 +38,9 @@ OUTPUT_DIR=sdk/go/
 CWD=$(pwd)
 # Build contracts
 # cd ${CONTRACTS_PATH}
-forge inspect "${NAME}" abi > ${OUTPUT_DIR}/${TYPE}.abi
-forge inspect "${NAME}" bytecode > ${OUTPUT_DIR}/${TYPE}.bin
-forge inspect "${NAME}" deployedBytecode > ${CWD}/dist/${TYPE_LOWER}_deployed.hex
+forge inspect "${NAME}" abi >${OUTPUT_DIR}/${TYPE}.abi
+forge inspect "${NAME}" bytecode >${OUTPUT_DIR}/${TYPE}.bin
+forge inspect "${NAME}" deployedBytecode >${CWD}/dist/${TYPE_LOWER}_deployed.hex
 
 # Run ABIGEN
 #cd ${CWD}
